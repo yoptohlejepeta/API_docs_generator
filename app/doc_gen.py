a07b440293fe3a701748a072c09e2f2e6a074120
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-02-21 23:36:27 +0100

    create output

--- app/doc_gen.py
+++ app/doc_gen.py
@@ -2,6 +2,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.styles import getSampleStyleSheet
+import os
 
 from inputs import Title
 
@@ -14,7 +15,11 @@ def fill_document(make_title: Title):
     styleT = styles["Title"]
 
     title = Paragraph(make_title.title, styleT)
+    if make_title.author is None:
+        make_title.author = "DocGen"
     author = Paragraph(make_title.author, styleN)
     date = Paragraph(make_title.date, styleN)
+
+    os.makedirs(os.path.dirname("output/docs.pdf"), exist_ok=True)
 
     doc.build([title, author, date])
